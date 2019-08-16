9dee9419169591c8bc37b1d06449a22e8efd46c4
qitas <qitas@qitas.cn>
2019-08-16 10:33:33 +0800

    add more

--- examples/rdm6300/rdm6300.ino
+++ examples/rdm6300/rdm6300.ino
@@ -1,22 +1,28 @@
 #include <SoftwareSerial.h>
 #include <RDM6300.h>
 
-#define RFID_RX_PIN 2
-#define RFID_TX_PIN 3
+#define RFID_RX_PIN 5
+#define RFID_TX_PIN 4
 
-RDM6300 RFID(RFID_RX_PIN, RFID_TX_PIN);
+RDM6300 RDM(RFID_RX_PIN, RFID_TX_PIN);
 
 int tag;
 
 void setup() {
   Serial.begin(115200);
   Serial.println("Serial Ready");
+
 }
 
 void loop() {
-  if(RFID.isIdAvailable()) {
-    tag = RFID.readId();
-    Serial.print("ID:       ");
+  if(RDM.isIdUartAvailable()) {
+    //tag = RDM.readId();
+    Serial.print("isIdUartAvailable");
+    Serial.println(RDM.realTagString);
+  }
+  if(RDM.isIdAvailable()) {
+    tag = RDM.readId();
+    Serial.print("ID: ");
     Serial.println(tag,HEX);
   }
 
