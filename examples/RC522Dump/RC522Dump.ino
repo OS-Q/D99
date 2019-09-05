98dffa0a941200541676b69683882cbd7e43e0bb
qitas <qitas@qitas.cn>
2019-09-05 22:22:15 +0800

    del

--- examples/RC522Dump/RC522Dump.ino
+++ examples/RC522Dump/RC522Dump.ino
@@ -15,7 +15,7 @@
 #include <SPI.h>
 #include <MFRC522.h>
 
-#define RST_PIN         16         
+#define RST_PIN         5         
 #define SS_PIN          15        
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
